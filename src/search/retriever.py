dde953709da6c0f385cab8aff0f8142874fb6e1b
hoanganh <ngohoanganh300320@gmail.com>
2024-05-30 15:49:56 +0700

    handle null update_time docs, WIP: retriever

--- src/search/retriever.py
+++ src/search/retriever.py
@@ -14,34 +14,49 @@ class Retriever:
         self._index_persist = index_persist
 
     def get(self, search_entry: SearchEntry):
-        occurrences = []
-        for key in search_entry.words:
-            pointers = self._index_persist.retrieve(key)
-            occurrences.append(pointers)
+        key_idx_to_occurs = {}
+        for i, key in enumerate(search_entry.words):
+            occurrences = self._index_persist.retrieve(key)
+            occurrences = self._clean_up_occurrences(occurrences)
+            doc_id_to_ocs = defaultdict(set)
+            for oc in occurrences:
+                doc_id_to_ocs[oc.doc_id].add(oc.position)
+            key_idx_to_occurs[i] = doc_id_to_ocs
 
-    def _filter_outdated_pointers(
-            self,
-            pointers: List[Pointer],
-    ):
-        last_updated_time = None
-        pointers_with_na_update_time = []
-        for pointer in pointers:
-            if pointer.update_time is None:
-                pointers_with_na_update_time.append(pointer)
-            elif last_updated_time is None or datetime_str(last_updated_time) <= datetime_str(pointer.update_time):
-                last_updated_time = pointer.update_time
-        keep = []
-        for pointer in pointers:
-            if pointer.update_time is None:
-                continue
-            if datetime_str(pointer.update_time) == datetime_str(last_updated_time):
-                keep.append(pointer)
 
-    def _from_occurrences_to_actual_doc(
+    def extract_full_match(
             self,
-            occurrences: List[List[Pointer]],
+            search_entry: SearchEntry,
     ):
-        for i, pointers in enumerate(occurrences):
-            doc_to_occur = defaultdict(list)
+        for i in range(len(search_entry.words)):
+            pass
+
+
+    def _clean_up_occurrences(
+            self,
+            occurrences: List[Pointer],
+    ) -> List[Pointer]:
+        def filter_outdated_pointers(
+                pointers: List[Pointer],
+        ):
+            last_updated_time = None
             for pointer in pointers:
-                doc_to_occur[pointer.doc_id].append(pointer)
+                if pointer.update_time is None:
+                    continue  # TODO handle null datetime
+                elif last_updated_time is None or datetime_str(last_updated_time) <= datetime_str(pointer.update_time):
+                    last_updated_time = pointer.update_time
+            keep = []
+            for pointer in pointers:
+                if pointer.update_time is None:
+                    continue
+                if datetime_str(pointer.update_time) == datetime_str(last_updated_time):
+                    keep.append(pointer)
+            return keep
+
+        doc_to_occur = defaultdict(list)
+        for oc in occurrences:
+            doc_to_occur[oc.doc_id].append(oc)
+        cleaned_occurrences = []
+        for ocs in doc_to_occur.values():
+            cleaned_occurrences.extend(filter_outdated_pointers(ocs))
+        return cleaned_occurrences

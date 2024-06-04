b1defa5882144d771bce2898b73dd322bb2299fe
hoanganh <ngohoanganh300320@gmail.com>
2024-06-04 18:27:40 +0700

    implemented retriever algo, TODO testing

--- src/search/retriever.py
+++ src/search/retriever.py
@@ -6,6 +6,10 @@ from src.common.utils import datetime_str
 from src.index.index_persist import IndexPersistent, Pointer
 from src.search.objects import SearchEntry
 
+
+KEYS_DISTANCE = [0, 1, 2]
+
+
 @dataclass
 class Occurrence:
     start: int
@@ -38,23 +42,55 @@ class Retriever:
     ):
         self._index_persist = index_persist
 
-    def get(self, search_entry: SearchEntry):
+    def get(self, search_entry: SearchEntry) -> List[int]:
+        """
+        :param search_entry:
+        :return: list of document_id
+        """
+        doc_candidates = self._build_doc_candidates(search_entry)
+        accepted_candidates = []
+        for doc_id, candidate in doc_candidates.items():
+            occurrences: List[Occurrence] = list(candidate.occurrences.values())
+            occurrences.sort(key=lambda o: (o.start, o.end))
+            word_tail_link = self.build_word_tail_link(occurrences)
+            f = [[False] * len(search_entry.words) for _ in range(len(occurrences))]
+            for i, oc in enumerate(occurrences):
+                for distance in KEYS_DISTANCE:
+                    tail = oc.start - distance
+                    if tail not in word_tail_link:
+                        continue
+                    links = word_tail_link[tail]
+                    for link in links:
+                        prev = occurrences[link]
+                        for key in oc.match_keys:
+                            if key == 0:
+                                f[i][key] = True
+                                continue
+                            if (key - 1) in prev.match_keys:
+                                f[i][key] = f[link][key - 1] or f[i][key]
+
+                if f[i][len(search_entry.words) - 1]:
+                    accepted_candidates.append(doc_id)
+                    continue
+        return accepted_candidates
+
+    def build_word_tail_link(self, occurrences: List[Occurrence]):
+        links = defaultdict(list)
+        for i, oc in enumerate(occurrences):
+            links[oc.end].append(i)
+        return links
+
+    def _build_doc_candidates(
+            self,
+            search_entry: SearchEntry,
+    ) -> Dict[int, DocumentCandidate]:
         doc_candidates = defaultdict(DocumentCandidate)
         for i, key in enumerate(search_entry.words):
             pointers = self._index_persist.retrieve(key)
             pointers = self._clean_up_pointers(pointers)
             for pointer in pointers:
                 doc_candidates[pointer.doc_id].add(i, key, pointer)
-
-        for doc_id, candidate in doc_candidates.items():
-            occurrences: List[Occurrence] = list(candidate.occurrences.values())
-            occurrences.sort(key=lambda o: (o.start, o.end))
-            f = [[False] * len(search_entry.words) for _ in range(len(occurrences))]
-            for i, oc in enumerate(occurrences):
-                for j, _ in enumerate(search_entry.words):
-
-
-
+        return doc_candidates
 
     def extract_full_match(
             self,
@@ -62,7 +98,6 @@ class Retriever:
     ):
         for i in range(len(search_entry.words)):
             pass
-
 
     def _clean_up_pointers(
             self,
